de87c4f50ac6a3ca8454684ec8561a91bf121691
bguneys <42909556+bguneys@users.noreply.github.com>
2021-02-17 23:19:09 +0300

    Search mechanism revised

--- src/reports/SearchManager.js
+++ src/reports/SearchManager.js
@@ -1,8 +1,21 @@
 import React, { useCallback, useState, useEffect } from "react";
 import {app} from './base.js'
 import { useRef } from "react";
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
 export default ({ reports: reports = [], onCreateNewReport: onCreateNewReport }) => {
+
+    const classes = useStyles();
 
     // UseStates. Whenever setter method is called screen re-renders with updated values
     const [newReportTitle, setNewReportTitle] = useState("");
@@ -19,31 +32,45 @@ export default ({ reports: reports = [], onCreateNewReport: onCreateNewReport })
       setNewReportTitle(event.target.value);
     }, []);
 
-    // Search button callback.. IF nothing is written inside Search input field then UseCallback doesn't call and 
-    // it doesn't assign reports list to queryReportList. There needs to be a change in Serch input to useCallack call.
-    const onAddReport = useCallback(
-      (event) => {
+    // Search button callback.. IF nothing is written inside Search input field then it brings all items and 
+    // it doesn't assign reports list to queryReportList. I removed USeCallback because it was respnding only to changes
+    // but at start we don't have any changes so it wouldn't call the methods.
+    const onAddReport = ((event) => {
         event.preventDefault();
 
-        // I added useState for queriedReportList because whenever a new list is queried we need to update the table
-        setQueriedReportList(reports.filter(([key, value]) => value.title == newReportTitle));
-        console.log(queriedReportList);
-        console.log(newReportTitle);
+        // check if there is any text inside search field before applying search
+        if (newReportTitle) {
+            // I added useState for queriedReportList because whenever a new list is queried we need to update the table
+            setQueriedReportList(reports.filter(([key, value]) => value.title == newReportTitle));
+            console.log(queriedReportList);
+            console.log(newReportTitle);
 
-      },
-      [newReportTitle, newReportDescription, newReportUrl]
-    );
+        // if there is no text in search field then fetch all the items
+        } else {
+            setQueriedReportList(reports);
+        }
+      });
+
+    useEffect(() => {
+
+          // The code where we create/add item into database
+          setQueriedReportList(reports);
+          console.log(queriedReportList);
+
+    }, [reports]); // Only re-run the effect if newReportUrl changes
 
     return (
         <div>
             <form>
 
                 <div className="container">
-                    <input
-                    type="text"
-                    placeholder="Search title.."
-                    value={newReportTitle}
-                    onChange={onNewReportTitleChange} />
+                    <TextField
+                        id="outlined-read-only-input"
+                        label="Arama"
+                        helperText="Başlıkta geçen metni arayınız"
+                        value={newReportTitle}
+                        onChange={onNewReportTitleChange}
+                        variant="outlined" />
                 </div>
 
                 <div className="container">
